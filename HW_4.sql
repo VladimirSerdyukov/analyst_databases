19cc8ac76a49bea62d3131db11834844ada4f2d5
VladimirSerdyukov <94453309+VladimirSerdyukov@users.noreply.github.com>
2021-12-13 19:11:44 +0300

    Create HW_4.sql

---
+++ HW_4.sql
@@ -0,0 +1,45 @@
+with
+	RFM(user_id, r,f,m) as 
+	# формируем таблицу пользователей с индексами
+		(select user_id,
+		CASE 
+		    WHEN count(id_o) > 5 THEN '3'
+		    WHEN count(id_o) > 2 THEN '2'
+		    ELSE '1'
+		    END AS f,
+		CASE 
+		    WHEN datediff('2017-12-31', max(o_date)) < 30 THEN '3'
+		    WHEN datediff('2017-12-31', max(o_date)) < 60 THEN '2'
+		    ELSE '1'
+		    END AS r,
+		CASE 
+		    WHEN SUM(price) > 15000 THEN '3'
+		    WHEN SUM(price) > 10000 THEN '2'
+		    ELSE '1'
+		    END AS m
+		from orders group by user_id),
+	assign_rfm(rfm, user_id) as
+	# присваиваем пользователям сегмент RFM
+		(select concat(r,f,m) as rfm, user_id from RFM),
+	group_rfm(rfm, count_user, turnover) as
+	# групперуем по рфм
+		(select t.rfm, count(tt.user_id), sum(tt.price) from orders as tt left join assign_rfm as t on t.user_id = tt.user_id 
+		group by t.rfm),
+	group_cat(group_c, count_user, turnover) as	
+	# vip, lost and regular
+		(select
+		CASE 
+		    WHEN rfm = 333 THEN 'yip'
+		    WHEN rfm = 233 THEN 'yip'
+		    WHEN rfm = 111 THEN 'lost'
+		    ELSE 'regular'
+		    END AS group_c,
+		    sum(count_user) as count_user,
+		    sum(turnover) as turnover
+		from group_rfm group by group_c),
+	test(sours, count_u, sum_price) as
+	# проверка
+		(select 'original' as sours, count(user_id) as o_count_u, sum(price) as o_sum_price from orders 
+		union all
+		select 'function' as sours, sum(count_user) as o_count_u, sum(turnover) as o_sum_price from group_cat)
+select * from test;
